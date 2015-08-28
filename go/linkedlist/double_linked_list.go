26887891308cdbbcf54e84689a11dcc078c5be2e
sshao <sshao@users.noreply.github.com>
2015-08-28 11:42:37 -0500

    [dll][go] add deletion, use testify

--- go/linkedlist/double_linked_list.go
+++ go/linkedlist/double_linked_list.go
@@ -31,3 +31,28 @@ func (l *DoubleLinkedList) insert(data interface{}) {
   new_node.Prev = cur_node
   cur_node.Next = new_node
 }
+
+func (l *DoubleLinkedList) delete(data interface{}) {
+  if l.Head.Data == data {
+    l.Head = l.Head.Next
+    if l.Head != nil {
+      l.Head.Prev = nil
+    }
+    return
+  }
+
+  prev_node := l.Head
+  cur_node := l.Head
+
+  for cur_node.Next != nil && cur_node.Data != data {
+    prev_node = cur_node
+    cur_node = cur_node.Next
+  }
+
+  if cur_node.Data == data {
+    if cur_node.Next != nil {
+      cur_node.Next.Prev = prev_node
+    }
+    prev_node.Next = cur_node.Next
+  }
+}
--- go/linkedlist/double_linked_list_test.go
+++ go/linkedlist/double_linked_list_test.go
@@ -1,22 +1,45 @@
 package linked_list
 
 import "testing"
+import "github.com/stretchr/testify/assert"
+import "reflect"
+
+func assertBothWays(t *testing.T, list *DoubleLinkedList, expected interface{}) {
+  slice := reflect.ValueOf(expected)
+
+  cur_node := list.Head
+  i := 0
+
+  for cur_node != nil {
+    assert.Equal(t, slice.Index(i).Interface(), cur_node.Data)
+    cur_node = cur_node.Next
+    i++
+  }
+
+  cur_node = list.Head
+  for cur_node.Next != nil {
+    cur_node = cur_node.Next
+  }
+  i = slice.Len() - 1
+
+  for cur_node != nil {
+    assert.Equal(t, slice.Index(i).Interface(), cur_node.Data)
+    cur_node = cur_node.Prev
+    i--
+  }
+}
 
 func TestCreateDouble(t *testing.T) {
   list := NewDoubleLinkedList()
 
-  if list.Head != nil {
-    t.Errorf("did not create an empty LinkedList")
-  }
+  assert.Nil(t, list.Head)
 }
 
 func TestInsertDouble(t *testing.T) {
   list := NewDoubleLinkedList()
   list.insert("hello")
 
-  if list.Head.Data != "hello" {
-    t.Errorf("list.Head.Data == %v, expected %v", list.Head.Data, "hello")
-  }
+  assert.Equal(t, list.Head.Data, "hello")
 }
 
 func TestInsertLotsDouble(t *testing.T) {
@@ -29,11 +52,11 @@ func TestInsertLotsDouble(t *testing.T) {
 
   cur_node := list.Head
 
-  for i,e := range expected {
-    if cur_node.Data != e {
-      t.Errorf("cur_node.Data at index %v == %v, expected %v", i, cur_node.Data, e)
-    }
+  i := 0
+  for cur_node != nil {
+    assert.Equal(t, cur_node.Data, expected[i])
     cur_node = cur_node.Next
+    i++
   }
 
   cur_node = list.Head
@@ -41,10 +64,95 @@ func TestInsertLotsDouble(t *testing.T) {
     cur_node = cur_node.Next
   }
 
-  for i := len(expected)-1; i >= 0; i-- {
-    if cur_node.Data != expected[i] {
-      t.Errorf("cur_node.Data at index %v == %v, expected %v", i, cur_node.Data, expected[i])
-    }
+  i = len(expected) - 1
+
+  for cur_node != nil {
+    assert.Equal(t, cur_node.Data, expected[i])
     cur_node = cur_node.Prev
+    i--
   }
+}
+
+func TestDeleteFirstDouble(t *testing.T) {
+  list := NewDoubleLinkedList()
+  insert := []int{1, 2, 3, 4, 5}
+  expected := []int{2, 3, 4, 5}
+
+  for _,e := range insert {
+    list.insert(e)
+  }
+
+  list.delete(1)
+
+  cur_node := list.Head
+  i := 0
+
+  for cur_node.Next != nil {
+    assert.Equal(t, cur_node.Data, expected[i])
+    cur_node = cur_node.Next
+    i++
+  }
+
+  cur_node = list.Head
+  for cur_node.Next != nil {
+    cur_node = cur_node.Next
+  }
+  i = len(expected) - 1
+
+  for cur_node != nil {
+    assert.Equal(t, cur_node.Data, expected[i])
+    cur_node = cur_node.Prev
+    i--
+  }
+}
+
+func TestDeleteLastDouble(t *testing.T) {
+  list := NewDoubleLinkedList()
+  insert := []int{1, 2, 3, 4, 5}
+  expected := []int{1, 2, 3, 4}
+
+  for _,e := range insert {
+    list.insert(e)
+  }
+
+  list.delete(5)
+
+  assertBothWays(t, list, expected)
+}
+
+func TestDeleteMiddleDouble(t *testing.T) {
+  list := NewDoubleLinkedList()
+  insert := []int{1, 2, 3, 4, 5}
+  expected := []int{1, 2, 4, 5}
+
+  for _,e := range insert{
+    list.insert(e)
+  }
+
+  list.delete(3)
+
+  assertBothWays(t, list, expected)
+}
+
+func TestDeleteOnly(t *testing.T) {
+  list := NewDoubleLinkedList()
+  list.insert("hello")
+
+  list.delete("hello")
+
+  assert.Nil(t, list.Head)
+}
+
+func TestDeleteNonexistent(t *testing.T) {
+  list := NewDoubleLinkedList()
+  insert := []int{1, 2, 3, 4, 5}
+  expected := insert
+
+  for _,e := range insert{
+    list.insert(e)
+  }
+
+  list.delete(9)
+
+  assertBothWays(t, list, expected)
 }
